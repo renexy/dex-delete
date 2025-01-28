faf0ded12cc789adf1f561e1801eadec31c20f2f
rene.vucko <rene.vucko@comtradegaming.com>
2025-01-28 14:25:02 +0100

    Trade dialog design

--- src/components/TradeDialog/TradeDialog.tsx
+++ src/components/TradeDialog/TradeDialog.tsx
@@ -1,37 +1,67 @@
-interface TradeDialogProps {
-    isOpen: boolean;
-    onClose: () => void;
-  }
+import Button from "../Button";
 
-  const TradeDialog: React.FC<TradeDialogProps> = ({ isOpen, onClose }) => {
+interface TradeDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const TradeDialog: React.FC<TradeDialogProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div 
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50" 
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
       onClick={onClose}
     >
-      <div 
-        className="bg-white p-6 rounded-lg shadow-lg w-[324px]" 
+      <div
+        className="bg-white px-[24px] pt-[12px] pb-[24px] rounded-[12px] shadow-lg w-[324px]"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-xl font-semibold mb-4">Dialog</h2>
-        <input 
-          type="text" 
-          placeholder="First Input" 
-          className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-        />
-        <input 
-          type="text" 
-          placeholder="Second Input" 
-          className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-        />
-        <button 
-          onClick={onClose} 
-          className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-        >
-          Close
-        </button>
+        <div className="flex flex-col gap-[12px] w-full">
+          <h2 className="font-semiboldtext-right cursor-pointer text-right">
+            X
+          </h2>
+          <div className="relative">
+            <div className="flex items-center w-full h-[48px] bg-[#0000000A] rounded-[4px]">
+              <input
+                type="text"
+                className="flex-1 text-black bg-transparent pr-[10px] text-right focus:outline-none"
+                placeholder="Enter amount"
+              />
+              <span className="text-[#74CDDC] top-[1px] relative text-[12px] font-bold mr-[12px]">
+                EUR
+              </span>
+            </div>
+            <div className="absolute bottom-[8px] left-3 right-[46px] border-b border-[#0000003D]"></div>
+          </div>
+          <div className="relative">
+            <div className="flex items-center w-full h-[48px] bg-[#0000000A] rounded-[4px]">
+              <input
+                type="text"
+                className="flex-1 text-black bg-transparent pr-[10px] text-right focus:outline-none"
+                placeholder="Enter amount"
+              />
+              <span className="text-[#74CDDC] top-[1px] relative text-[12px] font-bold mr-[12px]">
+                BTC
+              </span>
+            </div>
+            <div className="absolute bottom-[8px] left-3 right-[46px] border-b border-[#0000003D]"></div>
+          </div>
+          <div className="flex w-full items-center w-full gap-[24px] pt-[12px]">
+            <Button
+              callback={() => {
+                console.log("test");
+              }}
+              buttonText="Buy"
+            ></Button>
+            <Button
+              callback={() => {
+                console.log("test");
+              }}
+              buttonText="Sell"
+            ></Button>
+          </div>
+        </div>
       </div>
     </div>
   );
